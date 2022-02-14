1691f47551566d36f27f0efd1333d41164f9980e
Noah Gift <noah.gift@gmail.com>
2022-02-14 17:57:09 +0000

    adding code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # logging-monitoring-instrumentation
 A brief repository on logging monitoring and instrumentation in Python
+
+## Items for you to solve
+
+* Toggle log levels from debug to info to etc...
+
---
+++ daemon.py
@@ -0,0 +1,19 @@
+from random import choices
+from time import sleep
+
+#LOGGING
+import logging.config
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('simpleExample')
+
+
+FRUIT = ["apple", "cherry", "strawberry"]
+TIME = 2
+
+while True:
+    logging.info("Looping Daemon")
+    logging.info(f"This is INFO fruit: {choices(FRUIT)}")
+    logging.debug(f"This is DEBUG fruit: {choices(FRUIT)}")
+    print(f"Sleeping for {TIME}")
+    sleep(TIME)
+    
